ab4649ddc7b4824c0f5fb28a616174711eaa3edc
moshOntong-IT <57244338+moshOntong-IT@users.noreply.github.com>
2024-07-21 13:16:15 +0800

    Update function_service.dart

--- lib/services/function_services/function_service.dart
+++ lib/services/function_services/function_service.dart
@@ -1,9 +1,7 @@
 import 'package:appwrite_workbench/models/project.dart';
 import 'package:dart_appwrite/dart_appwrite.dart';
 
-sealed class FunctionServiceBase<T extends Function> {
-
-}
+sealed class FunctionServiceBase<T extends ProjectWorkbench> {}
 
 abstract class FunctionService<T extends ProjectWorkbench>
     extends FunctionServiceBase<T> {}
